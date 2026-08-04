cb100ab938517f5a604d2424d4cf6f47626ffbbd
Miriam Polzer <mpolzer@google.com>
2026-08-04 10:17:25 +0200

    Refactor word typeclasses

--- src/riscv/Utility/Words32Naive.v
+++ src/riscv/Utility/Words32Naive.v
@@ -2,12 +2,13 @@ Require Import Coq.ZArith.BinInt.
 Require Import coqutil.Z.Lia.
 Require Import coqutil.Word.Properties.
 Require Import coqutil.Word.Bitwidth.
-Require Import coqutil.Word.Naive.
+Require Export coqutil.Word.Naive.
 
 Local Open Scope Z_scope.
 
-#[global] Instance word: word.word 32 := Naive.word 32.
+#[export] Hint Extern 0 (word.word 32) => exact (Naive.word 32) : typeclass_instances.
+Notation word := (Naive.word 32).
 
-#[global] Instance Words32Naive: Bitwidth 32 := {|
+#[export] Instance Words32Naive: Bitwidth 32 := {|
   width_cases := or_introl eq_refl;
 |}.
